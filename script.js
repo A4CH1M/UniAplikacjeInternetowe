6e2b29dfe60b40b719fa5941e10ecc2fdf5cd3e5
Aachim <46716799+A4CH1M@users.noreply.github.com>
2024-10-26 12:06:00 +0200

    centered #grid-container elements; added comments in script

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.pn
 
 L.control.zoom({ position: 'topright' }).addTo(map);
 
+// requests permission to use notifications
 const requestNotificationPermission = () => {
     if ('Notification' in window) {
         Notification.requestPermission().then((permission) => {
@@ -15,6 +16,7 @@ const requestNotificationPermission = () => {
     }
 };
 
+// sends puzzle completion notification to user
 const notifyUser = () => {
     const permission = localStorage.getItem('notificationPermission');
 
@@ -32,16 +34,17 @@ const getRandomInt = (max) => {
     return Math.floor(Math.random() * max);
 }
 
+// requests permission to use notifications after page loading
 window.addEventListener('load', () => {
     requestNotificationPermission();
 });
 
+// downloads current location and displays it in #map div
 document.getElementById('location-button').addEventListener('click', () => {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position) => {
             const lat = position.coords.latitude;
             const lon = position.coords.longitude;
-            //alert(`Twoja lokalizacja to: ${lat}, ${lon}`);
             L.marker([lat, lon]).addTo(map);
             map.setView([lat, lon], 13);
         });
@@ -50,6 +53,7 @@ document.getElementById('location-button').addEventListener('click', () => {
     }
 });
 
+// downloads map from #map div and creates puzzle pieces
 document.getElementById('download-button').addEventListener('click', () => {
     document.getElementById('loading-spinner').style.display = 'block';
 
@@ -73,30 +77,34 @@ document.getElementById('download-button').addEventListener('click', () => {
     });
 });
 
+// cuts image in canvas into 16 pieces and places them shuffled in #puzzle-container div
 const createPuzzle = (canvas) => {
-
+    // puzzle sizes
     const puzzleWidth = 150;
     const puzzleHeight = 100; 
 
     const puzzleContainer = document.getElementById('puzzle-container');
-    puzzleContainer.innerHTML = '';
+    puzzleContainer.innerHTML = ''; // clear previous grid (in case there were puzzle pieces)
 
-
-    const puzzlePosition = [];
+    // add all posible multiplier permutations into an array
+    const offsetMultipliers = [];
     for (let i = 0; i < 4; i++) {
         for (let j = 0; j < 4; j++) {
-            puzzlePosition.push([i, j]);
+            offsetMultipliers.push([i, j]);
         }
     }
 
+    // puzzle shuffle
     for (let row = 0; row < 4; row++) {
         for (let col = 0; col < 4; col++) {
+            // cuts piece from canvas
             const pieceCanvas = document.createElement('canvas');
             pieceCanvas.width = puzzleWidth;
             pieceCanvas.height = puzzleHeight;
             const context = pieceCanvas.getContext('2d');
             context.drawImage(canvas, col * puzzleWidth, row * puzzleHeight, puzzleWidth, puzzleHeight, 0, 0, puzzleWidth, puzzleHeight);
 
+            // create next puzzle
             const piece = document.createElement('div');
             piece.id = `${row * 4 + col}`;
             piece.className = 'puzzle-piece';
@@ -106,15 +114,14 @@ const createPuzzle = (canvas) => {
 
             piece.style.position = 'absolute';
 
-            const pos = getRandomInt(puzzlePosition.length);
-
-            piece.style.top = `${puzzlePosition[pos][0] * puzzleHeight}px`;
-            piece.style.left = `${puzzlePosition[pos][1] * puzzleWidth}px`;
-
-            puzzlePosition.splice(pos, 1);
+            // get offset multiplier and then calculate position in #puzzle-container div
+            const pos = getRandomInt(offsetMultipliers.length);
+            piece.style.top = `${offsetMultipliers[pos][0] * puzzleHeight}px`;
+            piece.style.left = `${offsetMultipliers[pos][1] * puzzleWidth}px`;
+        
+            offsetMultipliers.splice(pos, 1); // remove used offset multiplier from list
 
             piece.draggable = true;
-
             piece.addEventListener('dragstart', handleDragStart);
 
             puzzleContainer.appendChild(piece);
@@ -132,6 +139,7 @@ const handleDragOver = (event) => {
     event.preventDefault();
 }
 
+// function creates visible grid in drop-zone so that it is clear where destination cells are
 const createDropZoneGrid = () => {
     const dropZone = document.getElementById('drop-zone');
     dropZone.innerHTML = '';
@@ -146,6 +154,7 @@ const createDropZoneGrid = () => {
     }
 }
 
+// drag&drop behaviour after dropping a puzzle
 const handleDropToGrid = (event) => {
     event.preventDefault();
     const dropCell = event.target;
@@ -154,18 +163,19 @@ const handleDropToGrid = (event) => {
         return;
     }
 
+    // if puzzle is placed on emplty cell in drop-zone
     if (dropCell.className === 'drop-cell') {
         dropCell.appendChild(draggedPiece);
     }
+    // if puzzle is placed on another puzzle
     else if (dropCell.className === 'puzzle-piece') {
         if (draggedPiece === dropCell) {
             return;
         }
 
+        // switch their ids and map image piece
         const tmpId = draggedPiece.id;
         const tmpBackground = draggedPiece.style.backgroundImage;
-        const top = draggedPiece.style.top;
-        const left = draggedPiece.style.left;
 
         draggedPiece.id = dropCell.id;
         draggedPiece.style.backgroundImage = dropCell.style.backgroundImage;
@@ -173,6 +183,7 @@ const handleDropToGrid = (event) => {
         dropCell.id = tmpId;
         dropCell.style.backgroundImage = tmpBackground;
 
+        // if the piece dragged was not in drop-cell
         if (draggedPiece.parentElement.id === 'puzzle-container') {
             return;
         }
@@ -182,6 +193,7 @@ const handleDropToGrid = (event) => {
     checkPuzzleCompletion();
 }
 
+// function combines puzzles into one image 
 const finishImage = (pieces) => {
     draggedPiece = null;
     for (let piece of pieces) {
@@ -200,23 +212,32 @@ const finishImage = (pieces) => {
 
 }
 
+// function checks wether the puzzle is arranged correctly
 const checkPuzzleCompletion = () => {
     let allCorrect = true;
 
     const pieces = document.getElementById('drop-zone').querySelectorAll('.puzzle-piece');
 
+    // check if all pieces are in #drop-zone div
     if (pieces.length != 16) {
         return;
     }
 
-    pieces.forEach((piece, index) => {
-        if (piece.id != index) {
+    // pieces.forEach((piece, index) => {
+    //     if (piece.id != index) {
+    //         allCorrect = false;
+    //     }
+    // });
+
+    for (let i = 0; i < pieces.length; i++) {
+        if (pieces[i].id != i) {
             allCorrect = false;
         }
-    });
+    }
 
     if (allCorrect) {
-        finishImage(pieces)        
+        finishImage(pieces)
+        // wait until event handler finishes and then send notification of puzlle completion     
         setTimeout(() => {
             notifyUser();
         }, 0);
